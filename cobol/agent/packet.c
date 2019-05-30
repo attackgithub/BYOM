34023173a6c69165f675a08f1967906fbd2a763f
mumbai <austinhudson@protonmail.ch>
2019-05-30 12:14:59 -0400

    Fix idiot mistakes

--- cobol/agent/packet.c
+++ cobol/agent/packet.c
@@ -1,17 +1,18 @@
 #include "packet.h"
+#include <stdlib.h>
 #define HEADER_LENGTH 8
 
-struct cobol_packet_new(uint32_t PacketType)
+struct CobolPacket * cobol_packet_new(uint32_t PacketType)
 {
-#ifndef _WIN64 || __WIN32
-    struct CobolPacket *pkt = calloc(1, sizeof(CobolPacket));
+#if _WIN64 || _WIN32
+    struct CobolPacket *pkt = calloc(1, sizeof(struct CobolPacket));
 #else
     struct CobolPacket *pkt = HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY,
 	sizeof(CobolPacket));
 #endif
     if (pkt != NULL) {
-	pkt->hdr.Type   = htonl(type);
-	pkt->hdr.Length = htonl(HEADER_LENGTH);
+	pkt->hdr.Type   = (uint32_t)PacketType;
+	pkt->hdr.Length = (uint32_t)8;
     }
     return pkt;
 }
