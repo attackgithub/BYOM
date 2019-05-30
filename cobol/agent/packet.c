5afbcee7c4ac3c95fec8472a6817e2e622d45965
mumbai <austinhudson@protonmail.ch>
2019-05-30 16:27:58 -0400

    Fix up some code

--- cobol/agent/packet.c
+++ cobol/agent/packet.c
@@ -1,15 +1,12 @@
+#include <windows.h>
 #include "packet.h"
 #include <stdlib.h>
 #define HEADER_LENGTH 8
 
 struct CobolPacket * cobol_packet_new(uint32_t PacketType)
 {
-#if _WIN64 || _WIN32
-    struct CobolPacket *pkt = calloc(1, sizeof(struct CobolPacket));
-#else
     struct CobolPacket *pkt = HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY,
-	sizeof(CobolPacket));
-#endif
+	sizeof(struct CobolPacket));
     if (pkt != NULL) {
 	pkt->hdr.Type   = (uint32_t)PacketType;
 	pkt->hdr.Length = (uint32_t)HEADER_LENGTH;
