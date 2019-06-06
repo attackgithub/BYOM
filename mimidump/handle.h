c7eafc718230289867dd3208192a40897f989480
mumbai <austinhudson@protonmail.ch>
2019-06-06 13:32:58 -0400

    more updates! Still a work in progress :/

--- mimidump/handle.h
+++ mimidump/handle.h
@@ -28,17 +28,82 @@
 
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
+#include <winternl.h>
 #include <ntstatus.h>
 
 #if defined(_MSC_VER)
 #pragma comment(lib, "ntdll.lib")
 #endif
 
-extern NTSTATUS NtQuerySystemInformation(
-   SYSTEM_INFORMATION_CLASS SystemInformationClass,
-   PVOID SystemInformation,
-   ULONG SystemInformationLength,
-   PULONG ReturnLength
+#define SystemHandleInformation 16
+#define ObjectBasicInformation  0
+#define ObjectNameInformation   1
+#define ObjectTypeInformation   2
+
+extern NTSTATUS NtDuplicateObject(
+   HANDLE SourceProcessHandle,
+   HANDLE SourceHandle,
+   HANDLE TargetProcessHandle,
+   PHANDLE TargetHandle,
+   ACCESS_MASK DesiredAccess,
+   ULONG Attributes,
+   ULONG Options
 );
+
+/*typedef struct _UNICODE_STRING {
+   USHORT Length;
+   USHORT MaximumLength;
+   LPWSTR Buffer;
+} UNICODE_STRING, *PUNICODE_STRING;
+
+typedef struct _SYSTEM_HANDLE {
+   ULONG  ProcessId;
+   BYTE   ObjectTypeNumber;
+   BYTE   Flags;
+   USHORT Handle;
+   PVOID  Object;
+   ACCESS_MASK GrantedAccess;
+} SYSTEM_HANDLE, *PSYSTEM_HANDLE;
+*/
+
+/*
+typedef struct _SYSTEM_HANDLE_INFORMATION {
+   ULONG HandleCount;
+   SYSTEM_HANDLE Handles[1];
+} SYSTEM_HANDLE_INFORMATION, *PSYSTEM_HANDLE_INFORMATION;*/
+
+typedef enum _POOL_TYPE {
+   NonPagedPool,
+   PagedPool,
+   NonPagedPoolMustSucceed,
+   DontUseThisType,
+   NonPagedPoolCacheAligned,
+   PagedPoolCacheAligned,
+   NonPagedPoolCacheAlignedMustS
+} POOL_TYPE, *PPOOL_TYPE;
+/*
+typedef struct _OBJECT_TYPE_INFORMATION {
+   UNICODE_STRING Name;
+   ULONG TotalNumberOfObjects;
+   ULONG TotalNumberOfHandles;
+   ULONG TotalPagedPoolStatus;
+   ULONG TotalNonPagedPoolUsage;
+   ULONG TotalNamePoolUsage;
+   ULONG TotalHandleTableUsage;
+   ULONG HighWaterNumberOfObjects;
+   ULONG HighWaterNumberOfHandles;
+   ULONG HighWaterPagedPoolUsage;
+   ULONG HighWaterNonPagedPoolUsage;
+   ULONG HighWaterNamePoolUsage;
+   ULONG HighWaterHandleTableUsage;
+   GENERIC_MAPPING GenericMapping;
+   ULONG ValidAccess;
+   BOOL  SecurityRequired;
+   BOOL  MaintainHandleCount;
+   USHORT MaintainTypeList;
+   POOL_TYPE PoolType;
+   ULONG PagedPoolUsage;
+   ULONG NonPagedPoolUsage;
+} OBJECT_TYPE_INFORMATION, *POBJECT_TYPE_INFORMATION;*/
 
 #endif
